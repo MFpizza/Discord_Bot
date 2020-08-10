01cb4279147e7b940e1baac9034c450e11b874c9
TwZhon <henry890811@gmail.com>
2020-08-11 00:30:02 +0800

    modify0811

--- bot.py
+++ bot.py
@@ -7,23 +7,19 @@ with open('setting.json', mode='r', encoding='utf-8') as jFile:
 
 bot = commands.Bot(command_prefix='~')
 
-
 @bot.event
 async def on_ready():
     print(">> Bot is online <<")
-
 
 @bot.event
 async def on_member_join(member):
     channel = bot.get_channel(690824514325708840)
     await channel.send(f'{member} join!')
 
-
 @bot.event
 async def on_member_remove(member):
     channel = bot.get_channel(690824514325708840)
     await channel.send(f'{member} leave!')
-
 
 @bot.command()
 async def speak(ctx):
@@ -45,6 +41,12 @@ async def 膝蓋(ctx):
 async def 夜雨落(ctx):
     await ctx.channel.send('上次跟我一起吃毒\n讓我知道他是史上最會開趴之人')
     pic = discord.File(jdata['Picture']+'1552499367332.jpg')
+    await ctx.channel.send(file=pic)
+
+@bot.command()
+async def all(ctx):
+    await ctx.channel.send('我說在座的各位都是垃圾')
+    pic = discord.File(jdata['Picture']+'maxresdefault.jpg')
     await ctx.channel.send(file=pic)
 
 bot.run(jdata['TOKEN'])
